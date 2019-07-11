17635ac4dc4451f68167c01e432eb9cb39a50e6d
amlane <twelvexstring@gmail.com>
2019-07-10 23:52:50 -0500

    added instructions for common element challenge

--- week-003.js
+++ week-003.js
@@ -24,3 +24,26 @@ function sortString(str) {
 console.log(sortString("dcba"));
 console.log(sortString("zycxbwa"));
 console.log(sortString("AzycxbCwBaA"));
+
+// Expected function: `commonElements`
+
+// Write a function called `commonElements` that has parameters for two arrays.  Return an array of all items that are present in both arrays.  Do not modify the arrays that are passed in.
+
+//  Input Example:
+// ```
+// [1, 2, 3, 4] [3, 4, 5, 6]
+// ['a', 'b', 'c'] ['x', 'y', 'z', 'a']
+// [1, 2, 3] [4, 5, 6]
+// ```
+// Output Example:
+// ```
+// [3, 4]
+// ['a']
+// []
+// ```
+// Required for submission:
+// ```
+// module.exports = {
+//   commonElements
+// }
+// ```
