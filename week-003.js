d6f26fc40657cb90faaea0e9786e62af673a5cb0
amlane <twelvexstring@gmail.com>
2019-07-25 23:06:03 -0500

    added code challenge 3 for week 3

--- week-003.js
+++ week-003.js
@@ -65,3 +65,12 @@ const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
 
 console.log(commonElements(arr1, arr2));
+
+// Good morning! Complete the function so that it converts dash-delimited ("kebab" case) & underscore-delimited ("snake" case) words into "camel" casing. The first word within the output should be capitalized only if the original word was capitalized.
+
+// toCamelCase("the-stealth-warrior")
+// returns "theStealthWarrior"
+// toCamelCase("The_stealth_warrior")
+// returns "TheStealthWarrior"
+
+function toCamelCase(str) {}
